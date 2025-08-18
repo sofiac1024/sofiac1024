db6ab281e9c0f277638d4b2e2bca791b0aa5e580
sofiac1024 <sofiacastro1024@gmail.com>
2025-08-18 23:32:54 +0200

    mapa funcional

--- dashboard/stations.py
+++ dashboard/stations.py
@@ -1,7 +1,12 @@
 import csv
+import pandas as pd
 import streamlit as st
+from pyproj import Transformer
 
 datafile = 'PUNTOS_PUBLICOS_RECARGA_VEHICULOS_ELECTRICOS.csv'
+
+# Configurar transformador: de EPSG:25830 (UTM Madrid) a EPSG:4326 (lat/lon WGS84)
+transformer = Transformer.from_crs("EPSG:25830", "EPSG:4326", always_xy=True)
 
 # Leer fichero csv y conseguir las coordenadas de cada punto de recarga
 def read_data():
@@ -12,14 +17,15 @@ def read_data():
             # Python entiende la coma decimal como un punto
             coorX = float(row['POINT_X'].replace(',', '.'))
             coorY = float(row['POINT_Y'].replace(',', '.'))
+            lon, lat = transformer.transform(coorX, coorY)
             points_data.append({
-                'longitude' : coorX,
-                'latitude' : coorY
+                'lon' : lon,
+                'lat' : lat
             })
-        return points_data
+    return pd.DataFrame(points_data)
 
 station_points = read_data()
 
 # Crear el mapa con los puntos encontrados
 st.header("EV Fast-Charginig Stations in Madrid (Spain)")
-st.map(station_points, latitude='latitude', longitude='longitude', zoom=8)
+st.map(station_points, latitude='lat', longitude='lon', zoom=10)
